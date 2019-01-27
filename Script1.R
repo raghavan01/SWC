889f4d7a9e4f485af3c9e1b68a5caf492a627874
varshalab <varsha.raghavan@med.nyu.edu>
2019-01-27 12:57:01 -0500

    trying out data

--- Script1.R
+++ Script1.R
@@ -1,1 +1,26 @@
 my_data <- c(2,4,6,4)
+str(my_data)
+my_data <- my_data*2
+my_data
+c(1,2,3)
+c('d','e','f')
+c(1,2,'f')
+typeof(c(1,2,'f'))
+coding <- data.frame(class1= c("bashshell", "introR"), 
+                     class2= c("git", "usingR"))
+coding
+str(coding)
+typeof(coding)
+codingv <- vector(mode = character, length = 2)
+?vector
+is.vector(coding)
+as.vector(coding)
+typeof(coding)
+
+alpha <- 1:26
+typeof(alpha)
+beta <- c(1:13)
+beta
+typeof(beta)
+alpha
+str(beta)
